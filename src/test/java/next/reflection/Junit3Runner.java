23f93ed96f422f7f4c88b14774d8648321f5c108
kwyoohae <gocksdl7979@gmail.com>
2023-07-12 16:45:37 +0900

    feat: 요구사항 2 구현

--- src/test/java/next/reflection/Junit3Runner.java
+++ src/test/java/next/reflection/Junit3Runner.java
@@ -1,11 +1,24 @@
 package next.reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 public class Junit3Runner {
     @Test
     public void runner() throws Exception {
         Class clazz = Junit3Test.class;
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
 
+        final Constructor constructor = clazz.getConstructor();
+        final Object Junit3Test = constructor.newInstance();
+
+        for (Method declaredMethod : declaredMethods) {
+            final String name = declaredMethod.getName();
+            if (name.startsWith("test")) {
+                declaredMethod.invoke(Junit3Test);
+            }
+        }
     }
 }
